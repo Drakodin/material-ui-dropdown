09a168d9c52abc1b0db4c21f8db0a32e7891b1eb
Tony Tian <57043533+Drakodin@users.noreply.github.com>
2020-08-05 14:51:26 -0500

    Add Dropdown and DropdownList

---
+++ DropdownList.js
@@ -0,0 +1,46 @@
+// Material-UI-DropdownList: Child Component to Material-UI-Dropdown
+import React from 'react';
+import { Popover, List, ListItem, ListItemIcon, ListItemText, Checkbox } from '@material-ui/core'
+
+const DropdownList = (props) => {
+    const open = (props.open) ? props.open : true;
+    const anchorEl = props.anchorEl;
+    const onClose = props.onClose;
+    const anchorOrigin = props.anchorOrigin;
+    const transformOrigin = props.transformOrigin;
+
+    const listItemSrc = props.src;
+    const target = props.target;
+    const listOnClick = props.listOnClick;
+
+    const style = props.style;
+    const listStyle = props.listStyle;
+    const boxStyle = props.boxStyle;
+
+    return (
+        <Popover
+            open={open}
+            style={style}
+            anchorEl={anchorEl}
+            onClose={onClose}
+            anchorOrigin={anchorOrigin}
+            transformOrigin={transformOrigin}
+        >
+            <List style={listStyle}>
+                {listItemSrc.map((value) => (
+                    <ListItem key={value} button onClick={listOnClick}>
+                        <ListItemIcon>
+                            <Checkbox style={boxStyle}
+                                checked={target.indexOf(value) !== -1}
+                                tabIndex={-1}
+                            />
+                        </ListItemIcon>
+                        <ListItemText primary={value}/>
+                    </ListItem>
+                ))}
+            </List>
+        </Popover>
+    )
+}
+
+export default DropdownList;

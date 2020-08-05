5e5fd21d48703be8c53b11e5971c68597c5945d5
Drakodin <tony.y.tian@gmail.com>
2020-08-05 15:29:32 -0500

    Add icons, Dropdown

--- Dropdown.js
+++ Dropdown.js
@@ -1,2 +1,31 @@
 import React from 'react';
 import { Typography, IconButton } from '@material-ui/core';
+import { ArrowDropDown, ArrowDropUp } from '@material-ui/icons';
+
+export default class Dropdown extends React.Component {
+    constructor(props) {
+        super(props);
+        this.anchorRef = React.createRef();
+    }
+
+    render() {
+        return(
+            <div 
+                ref={this.anchorRef}
+                className="d-flex flex-row align-items-center"
+                onClick={this.props.onClick}
+            >
+                <Typography className="ml-2 mr-2 no-text-select">{this.props.label}</Typography>
+                <IconButton className="cl-borderless" onClick={this.props.onClick}>
+                    {(!this.props.open)
+                        ? <ArrowDropDown/>
+                        : <ArrowDropUp/>
+                    }
+                </IconButton>
+                <div>
+                    {this.props.children(this)}
+                </div>
+            </div>
+        )
+    }
+}

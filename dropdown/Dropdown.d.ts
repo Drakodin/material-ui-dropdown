e543dda2e855a92a2a59b964abd8948d42c5b7ea
Drakodin <tony.y.tian@gmail.com>
2020-08-10 12:52:54 -0500

    Initial commit: typings.

---
+++ dropdown/Dropdown.d.ts
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+export interface DropdownProps {
+    open?: Boolean,
+    className?: String,
+    label?: String,
+
+    onClick: () => void,
+    onClose: () => void,
+    listOnClick: (value: any) => void,
+
+    target: Array<any>,
+    
+    style: CSSStyleDeclaration,
+    dropStyle: CSSStyleDeclaration,
+    listStyle: CSSStyleDeclaration,
+    boxStyle: CSSStyleDeclaration,
+}
+
+export default function Dropdown(props: DropdownProps): JSX.Element;
